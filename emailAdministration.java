9071d1ce4e12500052e521ea0546f88568e278f1
sadenrichards <74435560+sadenrichards@users.noreply.github.com>
2021-01-22 02:29:56 -0500

    Add files via upload

---
+++ emailAdministration.java
@@ -0,0 +1,12 @@
+package com.techvidvan.emailadministration;
+
+public class emailAdministration {
+
+	public static void main(String[] args) {
+		email em1 = new email("Sade","Richards");
+		System.out.println(em1.showInfo());
+	
+
+	}
+
+}
